45b8903df6ec301e1597001a1926882620c8cc8c
Hua Zan Mingz <phamquangminhex@gmail.com>
2023-05-09 01:38:03 +0700

    - Update build

--- cocos-js/wait-for-ammo-instantiation.js
+++
@@ -1,2 +0,0 @@
-System.register(["./instantiated-0a90480f.js"],(function(t){"use strict";return{setters:[function(e){t("default",e.h7)}],execute:function(){}}}));
-//# sourceMappingURL=wait-for-ammo-instantiation.js.map
